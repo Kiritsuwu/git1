aa3f7c1ca5267f193c7d1ede971ca1ec875f6ccc
261264 <261264@edu.p.lodz.pl>
2025-10-06 10:54:12 +0200

    02 Zmiana mnozenia w metodzie Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int Policz(int a, int b) {
+    return a*b;
+}
+
 int main() {
     cout << "Hello World!\n";
+    cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << endl;
     return 0;
 }
